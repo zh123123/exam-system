b6050e51814af65053dad5c46869b27bc5ed90b7
huan_glb <huan_glb@zh.com>
2019-04-22 22:55:15 +0800

    更新部分内容

--- src/main/java/com/Z/controller/ThymeleafController.java
+++ src/main/java/com/Z/controller/ThymeleafController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/th")
 public class ThymeleafController {
-
+	
 	@RequestMapping(value = { "/index", "/" })
 	public String index() {
 
